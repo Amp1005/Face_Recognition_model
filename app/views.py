e3d180b1bea3eac6da544ccf306d29464502d04f
Ashish <patilashish1005@gmail.com>
2026-02-06 08:57:30 +0530

    connecting to model

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 import cv2
 from app.face_recognition import faceRecognitionPipeline
 from flask import render_template, request
-
+import matplotlib.image as mating
 
 UPLOAD_FOLDER = 'static/upload'
 
@@ -23,6 +23,25 @@ def genderapp():
         filename = f.filename
         path = os.path.join(UPLOAD_FOLDER, filename)
         f.save(path)
+        pred_image, prediction = faceRecognitionPipeline(path)
+        pred_filename = 'prediction_image.jpg'
+        cv2.imwrite(f'./static/predict/{pred_filename}', pred_image)
+        print(prediction)
+        report = []
 
-         
-    return render_template('gender.html')
+        for i, obj in enumerate(prediction):
+            gray_image = obj['roi'] 
+            eigen_image = obj['eig_img'].reshape(100,100)
+            gender_name = obj['prediction_name']
+            score = round(obj['score']*100,2)
+
+            gray_image_name = f'roi_{i}.jpg'
+            eig_image_name = f'eigen_{i}.jpg'
+            mating.imsave(f'./static/predict/{gray_image_name}', gray_image, cmap='gray')
+            mating.imsave(f'./static/predict/{eig_image_name}', eigen_image, cmap='gray')
+
+            report.append([gray_image_name, eig_image_name, gender_name, score])
+    
+        return render_template('gender.html', fileupload=True, report=report) #GET
+    return render_template('gender.html',  fileupload= False) #POST
+
